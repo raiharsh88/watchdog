f85718f9225a40a0e6b14a803fee72344c95f1a8
raiharsh88 <raiharsh88@gmail.com>
2021-03-28 14:35:13 +0000

    updating readme images

--- dashboard/config.js
+++ dashboard/config.js
@@ -1,5 +1,4 @@
 module.exports = {
   mongoURI: "mongodb://harsh:password@localhost:27017/wd",
-  sgKey:
-    "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",
+  sgKey: "",
 };
